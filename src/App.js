45745d3aacee93638c887705b6c2f60b2b86dbea
Laurat HaAga <l4tt1haga@gmail.com>
2021-10-04 16:04:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Cards from './components/Cards';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Try without "https://cors-anywhere.herokuapp.com/" see if it works!
+//Link modified because of cross origin resource sharing 
+const url = 'https://cors-anywhere.herokuapp.com/https://api.first.org/data/v1/teams';
+
+const App = () => {
+    const [teams, setTeams] = useState([]);
+    const [searchTeam, setSearchTeam] = useState('');
+    const [sortTeams, setSortTeams] = useState(true);
+
+    //Send the sorted teams to render
+    const sorted = teams.sort((a, b) => {
+        const {"team-full": t_f1} = a;
+        const {"team-full": t_f2} = b;
+        const isReversted = (sortTeams === false) ? 1 : -1;
+        return isReversted * t_f1.localeCompare(t_f2);
+    });
+
+    useEffect(() => {
+
+        (async function() {
+            try{
+                const response = await axios.get(url); 
+                const data = await response.data.data;
+                setTeams(data);
+            }
+            catch(e){
+                console.log(e);   
+            }
+        }());
+        
+    },[]);
+    
+    return (
+        <div className="container" >
+            <Header 
+                setSearchTeam={setSearchTeam} 
+                searchTeam={searchTeam}
+                sortTeams={sortTeams}
+                setSortTeams={setSortTeams}
+            />
+            <Cards sorted={sorted} searchTeam={searchTeam}/>
+        </div>
+    )
 }
 
 export default App;
